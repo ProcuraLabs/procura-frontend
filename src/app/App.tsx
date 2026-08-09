a2b7c535b149c2a7cf32b06096c436f2029d1689
KehiOla <kehindemicheal2022@gmail.com>
2026-08-10 00:32:33 +1000

    chore: establish frontend foundation

---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="app">
+      <h1>Procura</h1>
+      <p>Decentralized procurement &amp; milestone payments on Stellar.</p>
+    </main>
+  )
+}
